741a2011d80c3bc7f158d216db9225508d2ea52c
Zhili Yang <zhya1033@colorado.edu>
2015-02-22 15:44:24 -0700

    50/50, Zhili Yang

--- js/my/repos.js
+++ js/my/repos.js
@@ -3,5 +3,18 @@ function myRepos(){
     // display my repos in #list
     // e.g., https://api.github.com/users/doubleshow/repos
 
-    alert('todo')
+    $.get("https://api.github.com/users/zhya215/repos", github, function(data) {
+    	var myRepos = data
+
+    	$.get("/git-jquery/templates/repoList.jade", function(template){
+    		// render the template
+            var html = jade.render(template, {items: myRepos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            repoView(repos[0].full_name)
+    	}
+    }
 }
