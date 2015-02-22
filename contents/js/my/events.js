49cf9ae0ef7f5261ea9ec612cc32c54f77c4f20d
Zhili Yang <zhya1033@colorado.edu>
2015-02-22 15:56:11 -0700

    fixed my events

--- contents/js/my/events.js
+++ contents/js/my/events.js
@@ -12,6 +12,7 @@ function myEvents(){
 
             // assign the rendered html to the dom element whose id is #list
             $("#list").html(html)
+            $("#details").html('')
 
     	})
     })
