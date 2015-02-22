b677204494821b769b64513677a3371952123be2
Zhili Yang <zhya1033@colorado.edu>
2015-02-22 15:44:58 -0700

    build

--- js/my/events.js
+++ js/my/events.js
@@ -3,5 +3,16 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+        $.get("https://api.github.com/users/zhya215/events", github, function(data) {
+    	var events = data
+
+    	$.get("/git-jquery/templates/eventList.jade", function(template){
+    		// render the template
+            var html = jade.render(template, {items: events})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+    	})
+    })
 }
--- js/my/repo.js
+++ js/my/repo.js
@@ -3,5 +3,22 @@ function myRepo(full_name){
     // display a particular repo of mine in #details
     // e.g., https://api.github.com/repos/doubleshow/up
 
-    alert('todo')
+    console.log('viewing my repo: ', full_name)
+
+    // e.g., https://api.github.com/repos/ucdd2-sp15/nn-express
+
+    $.get("https://api.github.com/repos/" + full_name, github, function(data) {
+        
+        var repo = data
+        // console.log(repo)
+
+        $.get("/git-jquery/templates/repoView.jade", function(template) {
+
+            var html = jade.render(template, {item: repo})
+            
+            $("#details").html(html)
+
+        })
+
+    })
 }
